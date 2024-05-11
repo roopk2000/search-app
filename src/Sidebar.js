0446ae0b0a3f8fe757d93c45960bf8be9a7cce5e
Roop Kumar <108384154+roopk2000@users.noreply.github.com>
2024-05-11 16:38:59 +0530

    sidebar improved

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,20 +1,35 @@
-// Sidebar.js
-
 import React from 'react';
 
 const Sidebar = () => {
   return (
-    <div className="sidebar" style={{ position: 'fixed', left: 0, top: 0, bottom: 0, width: '200px', backgroundColor: '#fff', zIndex: 999 }}>
-      <h3>Brand</h3>
-      <ul>
-        <li>Mango</li>
-        <li>H&M</li>
-      </ul>
-      <h3>Price Range</h3>
-      <ul>
-        <li>Under 500</li>
-        <li>1000 To 3000</li>
-      </ul>
+    <div className="sidebar" style={{ position: 'fixed', left: 0, top: 0, bottom: 0, width: '200px', backgroundColor: '#fff', zIndex: 999, padding:"15px" }}>
+      <h3>BRAND</h3>
+      <hr /> {/* Line before BRAND */}
+      <input type="checkbox" /> Mango
+      <br />
+      <input type="checkbox" /> H&M
+      <h3>PRICE RANGE</h3>
+      <hr /> {/* Line before PRICE RANGE */}
+      <input type="checkbox" /> Under 500
+      <br />
+      <input type="checkbox" /> 1000 To 3000
+      <br />
+      <input type="checkbox" /> 3000 To 5000
+      <br />
+      <input type="checkbox" /> 5000 To 8000
+      <br />
+      <input type="checkbox" /> 8000 To 10000
+      <h3>RATINGS</h3>
+      <hr /> {/* Line before RATINGS */}
+      <input type="checkbox" /> ★★★★★
+      <br />
+      <input type="checkbox" /> ★★★★
+      <br />
+      <input type="checkbox" /> ★★★
+      <br />
+      <input type="checkbox" /> ★★
+      <br />
+      <input type="checkbox" /> ★
     </div>
   );
 };
